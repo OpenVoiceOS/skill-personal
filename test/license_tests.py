b7f936ce917d8ee8c9f8e4ed05715b4ea0ff00f6
jarbasai <jarbasai@mailfence.com>
2022-03-02 01:49:39 +0000

    workflows

--- test/license_tests.py
+++ test/license_tests.py
@@ -22,7 +22,7 @@ allow_unknown = False
 allow_unlicense = True
 allow_ambiguous = False
 
-pkg_name = "ovos-skill-personal"
+pkg_name = "ovos-skill-skill-personal"
 
 
 class TestLicensing(unittest.TestCase):
